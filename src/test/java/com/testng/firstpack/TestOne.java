72a2e750601bf9cc6229979b431a81d50ffca37b
Uma Nagarajan <unagarajan@planittesting.com>
2021-05-14 18:13:56 +1000

    first commit

---
+++ src/test/java/com/testng/firstpack/TestOne.java
@@ -0,0 +1,27 @@
+package com.testng.firstpack;
+
+
+	
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.Test;
+
+	import io.github.bonigarcia.wdm.WebDriverManager;
+
+	public class TestOne {
+		WebDriver driver;  
+		@Test
+		public void test1() {
+	 	 
+			WebDriverManager.chromedriver().setup();			
+			driver=new ChromeDriver();
+			driver.get("http://jupiter.cloud.planittesting.com/");
+			System.out.println("Got the title in TestNG"+ driver.getTitle());
+			driver.quit();
+			
+			
+		}
+	}
+
+
+
